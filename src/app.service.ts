a07d2500a766d143b0c3d04d59d58ec66e023d6d
Italo1033 <79849291+italo1033@users.noreply.github.com>
2024-07-31 23:21:45 -0300

    criando transaction

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { CreateTransactionDTO } from './dto/create-transaction.dto';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor( private readonly prismaService: PrismaService){
+    
+  }
+
+  async createTransaction ( data: CreateTransactionDTO){
+    const createTransaction = await this.prismaService.transaction.create({
+      data
+    })
+
   }
 }
